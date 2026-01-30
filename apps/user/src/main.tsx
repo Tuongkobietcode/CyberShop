1ebd182e8558a59a3dd9741022d24e5fb431f58e
hongdat-pham <phamhongdat314@gmail.com>
2026-01-30 14:10:05 +0700

    Build products/FilterSidebar

--- apps/user/src/main.tsx
+++ apps/user/src/main.tsx
@@ -1,8 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import FilterSidebar from "./pages/products/components/FilterSidebar";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <FilterSidebar />
   </StrictMode>,
 );
