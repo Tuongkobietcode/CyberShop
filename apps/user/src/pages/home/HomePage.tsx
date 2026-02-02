ceaecaebb9efcdc3974613c97973f4459e6cc879
Tuongkobietcode <duongtuong131004@gmail.com>
2026-02-02 20:37:40 +0700

    Building HomePage

--- apps/user/src/pages/home/HomePage.tsx
+++ apps/user/src/pages/home/HomePage.tsx
@@ -1,12 +1,14 @@
 // Ghép các section của trang home lại với nhau thôi k có logic nhé
 import HeroSection from "./components/HeroSection";
 import FeaturedCategorySection from "./components/FeaturedCategorySection"; 
+import ProductShowcaseSection from "./components/ProductShowcaseSection";
 
 export default function HomePage() {
   return (
     <div>
       <HeroSection />
       <FeaturedCategorySection />
+      <ProductShowcaseSection />
     </div>
   );
 }
---
+++ apps/user/src/pages/home/components/ProductShowcaseSection.tsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+
+export default function ProductShowcaseSection() {
+    return (
+        <section>
+            <div>
+                <h2>Browse By Category</h2>                
+            </div>
+            <div>
+                
+            </div>
+        </section>           
+    );
+}
