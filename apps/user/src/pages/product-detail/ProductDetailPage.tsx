c106f1d6a7e21c9cc686d0e80370aa352083e63d
Onotoki <chucuongtn@gmail.com>
2026-02-06 12:43:05 +0700

    build linh tinh

--- apps/user/src/pages/product-detail/ProductDetailPage.tsx
+++ apps/user/src/pages/product-detail/ProductDetailPage.tsx
@@ -1,4 +1,13 @@
 import { useState } from "react";
+import phone from "../../assets/images/Images.png";
+import screenIcon from "../../assets/images/Screensize.png";
+import cpuIcon from "../../assets/images/Cpu.png";
+import coreIcon from "../../assets/images/Core.png";
+import cameraIcon from "../../assets/images/Camera.png";
+import frontCameraIcon from "../../assets/images/FrontCamera.png";
+import batteryIcon from "../../assets/images/Battery.png";
+import { FiTruck, FiCheckCircle } from "react-icons/fi";
+import { MdOutlineStore } from "react-icons/md";
 
 const COLORS = [
   "bg-black",
@@ -10,6 +19,57 @@ const COLORS = [
 
 const STORAGES = ["128GB", "256GB", "512GB", "1TB"];
 
+const SPECS = [
+  {
+    label: "Screen size",
+    value: "6.7”",
+    img: screenIcon,
+  },
+  {
+    label: "CPU",
+    value: "Apple A16 Bionic",
+    img: cpuIcon,
+  },
+  {
+    label: "Number of Cores",
+    value: "6",
+    img: coreIcon,
+  },
+  {
+    label: "Main camera",
+    value: "48-12-12 MP",
+    img: cameraIcon,
+  },
+  {
+    label: "Front-camera",
+    value: "12 MP",
+    img: frontCameraIcon,
+  },
+  {
+    label: "Battery capacity",
+    value: "4323 mAh",
+    img: batteryIcon,
+  },
+];
+
+const SERVICES = [
+  {
+    title: "Free Delivery",
+    desc: "1-2 day",
+    icon: <FiTruck size={22} />,
+  },
+  {
+    title: "In Stock",
+    desc: "Today",
+    icon: <MdOutlineStore size={22} />,
+  },
+  {
+    title: "Guaranteed",
+    desc: "1 year",
+    icon: <FiCheckCircle size={22} />,
+  },
+];
+
 const ProductDetailPage = () => {
   const [color, setColor] = useState("bg-black");
   const [storage, setStorage] = useState("1TB");
@@ -17,13 +77,15 @@ const ProductDetailPage = () => {
   return (
     <div className="flex min-h-screen">
       {/* LEFT */}
-      <div className={`w-1/2 bg-black`}></div>
+      <div className="w-1/2 p-30">
+        <img src={phone} />
+      </div>
 
       {/* RIGHT */}
       <div className="w-1/2 bg-white p-10">
-        <div className="max-w-md space-y-6">
+        <div className="max-w-full space-y-6">
           {/* title */}
-          <h1 className="text-2xl font-bold">Apple iPhone 14 Pro Max</h1>
+          <h1 className="text-3xl font-bold">Apple iPhone 14 Pro Max</h1>
 
           {/* price */}
           <div className="flex items-center gap-3">
@@ -33,13 +95,14 @@ const ProductDetailPage = () => {
 
           {/* colors */}
           <div className="flex">
-            <p className="text-sm text-gray-500 pr-4">Select color:</p>
+            <p className="text-sm text-black mb-2 mr-4">Select color :</p>
             <div className="flex gap-3">
               {COLORS.map((c) => (
                 <button
+                  key={c}
                   onClick={() => setColor(c)}
                   className={`w-6 h-6 rounded-full ${c} ${
-                    color === c ? "ring-1 ring-black" : ""
+                    color === c ? "ring-2 ring-black" : ""
                   }`}
                 />
               ))}
@@ -48,12 +111,12 @@ const ProductDetailPage = () => {
 
           {/* storage */}
           <div>
-            <p className="text-sm text-gray-500 mb-2">Storage :</p>
-            <div className="flex gap-3 flex-wrap">
+            <div className="flex gap-3">
               {STORAGES.map((s) => (
                 <button
+                  key={s}
                   onClick={() => setStorage(s)}
-                  className={`px-4 py-2 rounded-lg border text-sm ${
+                  className={`px-4 py-2 w-full rounded-lg border-2 text-sm ${
                     storage === s
                       ? "border-black text-black"
                       : "border-gray-300 text-gray-400"
@@ -65,22 +128,56 @@ const ProductDetailPage = () => {
             </div>
           </div>
 
+          {/* specs */}
+          <div className=" grid grid-cols-3 gap-3 text-sm">
+            {SPECS.map((item) => (
+              <div
+                key={item.label}
+                className="flex items-center gap-3 rounded-xl p-4 bg-[#F4F4F4]"
+              >
+                <img src={item.img} className="w-6 h-6" />
+
+                <div>
+                  <p className="text-gray-400 text-xs">{item.label}</p>
+                  <p className="text-[#4E4E4E] font-medium">{item.value}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+
           {/* description */}
           <p className="text-gray-500 text-sm">
             Enhanced capabilities thanks to an enlarged display of 6.7 inches
             and work without recharging throughout the day.
           </p>
 
-          {/* specs */}
-
           {/* buttons */}
           <div className="flex gap-3">
-            <button className="flex-1 border py-3 rounded-lg">
+            <button className="flex-1 border-2 py-3 rounded-lg">
               Add to Wishlist
             </button>
             <button className="flex-1 bg-black text-white py-3 rounded-lg">
               Add to Cart
             </button>
+          </div>
+
+          {/* services */}
+          <div className="flex justify-between mt-6 gap-3">
+            {SERVICES.map((item) => (
+              <div
+                key={item.title}
+                className="flex items-center gap-3 px-4 py-3 rounded-xl flex-1"
+              >
+                <div className="w-15 h-15 rounded-xl bg-[#F7F7F7] flex items-center justify-center text-[#797979]">
+                  {item.icon}
+                </div>
+
+                <div className="text-sm">
+                  <p className="text-gray-400">{item.title}</p>
+                  <p className="font-medium text-black">{item.desc}</p>
+                </div>
+              </div>
+            ))}
           </div>
         </div>
       </div>
