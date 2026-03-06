e7e83cbbc0cfcfea23a66e596ca6b6995eb7dc93
Đức Minh Vũ <vum9848@gmail.com>
2026-03-06 13:20:48 +0700

    update package.lock

--- apps/user/src/App.tsx
+++ apps/user/src/App.tsx
@@ -1,17 +1,23 @@
 // import { OrderSummary } from "./pages/cart/components/OrderSummary";
-// import { CartList } from "./pages/cart/components/CartList";
-// import { SortBar } from "./pages/products/components/SortBar";
-// import { ShippingMethodList } from "./pages/checkout/shipping/components/ShippingMethodList";
+import { CartList } from "./pages/cart/components/CartList";
 // import CartPage from "./pages/cart/CartPage";
-import { RegisterPage } from "./pages/register/RegisterPage";
+
+// import { SortBar } from "./pages/products/components/SortBar";
+
+// import { ShippingMethodList } from "./pages/checkout/shipping/components/ShippingMethodList";
+
+// import { RegisterPage } from "./pages/register/RegisterPage";
 
 function App() {
     // return <OrderSummary />
-    // return <CartList />
-    // return <SortBar />
-    // return <ShippingMethodList />
+    return <CartList />
     // return <CartPage />
-    return <RegisterPage />
+    
+    // return <SortBar />
+
+    // return <ShippingMethodList />
+    
+    // return <RegisterPage />
 }
 
 export default App;
