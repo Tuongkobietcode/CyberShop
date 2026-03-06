3a05b5b6716eb4851a410d34d02e3484b6384b59
hongdat-pham <phamhongdat314@gmail.com>
2026-03-06 13:48:54 +0700

    Build UI Orderpage

--- apps/admin/src/main.tsx
+++ apps/admin/src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import OrdersListPage from "./features/orders/pages/OrdersListPage";
+import OrdersPage from "./features/orders/pages/OrdersPage";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <OrdersListPage />
+    <OrdersPage />
   </StrictMode>,
 );
